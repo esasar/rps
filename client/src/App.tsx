c742150927bf4e6325680cd4f1236a716393b944
Esa Särkelä <esa.sarkela@tuni.fi>
2024-10-01 18:19:00 +0300

    add frontend controls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 import { useState, useEffect, useRef } from 'react';
+import './App.css'
 
 const URL = 'http://localhost:8080';
 
@@ -15,7 +16,9 @@ function App() {
   const [playerId, setPlayerId] = useState<string>(''); 
   const [room, setRoom] = useState<Room>();
   const [joinRoomIdField, setJoinRoomIdField] = useState<string>('');
-  
+  const [result, setResult] = useState();
+  const [move, setMove] = useState('');
+
   const socket = useRef<Socket>();
 
   useEffect(() => {
@@ -36,6 +39,11 @@ function App() {
     socket.current.on('room:joined', (room: Room) => {
       setRoom(room);
     });
+
+    socket.current.on('game:result', (result: any) => {
+      setMove('');
+      setResult(result);
+    })
 
     return () => {
       if (socket.current) {
@@ -62,35 +70,73 @@ function App() {
     }
   };
 
+  const makeMoveButtonHandler = (move: Move) => {
+    if (socket.current) {
+      setMove(move);
+      socket.current.emit('game:move', move, room?.id);
+    }
+  }
+
   return (
     <main>
-      {/* display users id */}
-      <div>{playerId}</div>
-
-      {/* display room info and leave room button if user is in a room */}
-      {room && (
-        <div>
-          <div>Room ID: {room.id}</div>
-          <div>Players: {room.playerIds.join(', ')}</div>
-          <button onClick={leaveRoomButtonHandler}>Leave room</button>
-        </div>
-      )} 
-      
-      {/* display room creation and joining options if user is not in a room */}
-      {!room && (
-        <div>
-          <button onClick={createRoomButtonHandler}>Create Room</button>
+      <div className='section'>
+        {/* display room info and leave room button if user is in a room */}
+        {room && (
           <div>
-            <input 
-              value={joinRoomIdField}
-              onChange={(e) => setJoinRoomIdField(e.target.value)}
-              type='text'
-              placeholder='room id'
-            />
-            <button onClick={joinRoomButtonHandler}>Join room</button>
+            <div>Room ID: {room.id}</div>
+            <button onClick={leaveRoomButtonHandler}>Leave room</button>
+            <div>
+              <button 
+                className={`move ${(move ?? '') === 'rock' ? '' : ' grayscale'}`}
+                onClick={() => makeMoveButtonHandler('rock')}
+              >
+                {'\u270A'}
+              </button>
+              <button 
+                className={`move ${(move ?? '') === 'paper' ? '' : ' grayscale'}`}
+                onClick={() => makeMoveButtonHandler('paper')}
+              >
+                {'\u270B'}
+              </button>
+              <button 
+                className={`move ${(move ?? '') === 'scissors' ? '' : ' grayscale'}`}
+                onClick={() => makeMoveButtonHandler('scissors')}
+              >
+                {'\u270C\uFE0F'}
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )} 
+
+        {/* display room creation and joining options if user is not in a room */}
+        {!room && (
+            <div>
+              <button onClick={createRoomButtonHandler}>Create Room</button>
+              <div>
+                <input 
+                  value={joinRoomIdField}
+                  onChange={(e) => setJoinRoomIdField(e.target.value)}
+                  type='text'
+                  placeholder='room id'
+                />
+                <button onClick={joinRoomButtonHandler}>Join room</button>
+              </div>
+            </div>
+          )}      
+      </div>
+      
+      <div className='section'>
+        {(result) ? (
+          result?.winner === 'draw' ? 'Draw!' : result?.winner === playerId ? 'You win!' : 'You lost!'
+        ) : (!room) ? (
+          'Join or create a room!' 
+        ) : (room?.playerIds.length === 2) ? (
+          'Game ends when both have chosen xd'
+        ) : (
+          'Waiting for opponent'
+        )}
+      </div>
+      
     </main>
   )
 }
